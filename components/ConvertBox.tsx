f497f9c3c1e093ea66eb6ba7bd99b44da4e15dfb
Josh Pullen <JoshPullen27@gmail.com>
2022-11-24 21:17:20 -0500

    Allow uploading .sb3 files

--- components/ConvertBox.tsx
+++ components/ConvertBox.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import * as gtag from "../lib/gtag";
 import classNames from "classnames";
 
@@ -42,44 +42,152 @@ export default function ConvertBox() {
     }
   };
 
+  const onSubmitUpload = async (event) => {
+    event.preventDefault();
+
+    gtag.event({
+      category: "Translate Project",
+      action: "Edit in CodeSandbox",
+      label: "File",
+    });
+
+    setLoading(true);
+
+    const url = `/api/upload/codesandbox`;
+    const response = await fetch(url, {
+      method: "POST",
+      body: event.target.querySelector("input[type=file]").files[0],
+    });
+    const data = await response.json();
+
+    if (response.ok) {
+      window.location.href = data.url;
+    } else {
+      console.log(data);
+      setError({ status: response.status, info: data.error });
+      setLoading(false);
+    }
+  };
+
+  const uploadFormRef = useRef<HTMLFormElement>(null);
+
+  const [draggingFile, setDraggingFile] = useState(false);
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    setDraggingFile(true);
+  }, []);
+  const onDragLeave = useCallback((event) => {
+    event.preventDefault();
+    setDraggingFile(false);
+  }, []);
+  const onDrop = useCallback((event) => {
+    event.preventDefault();
+    setDraggingFile(false);
+    const fileInput: HTMLInputElement =
+      uploadFormRef.current?.querySelector("input[type=file]");
+    if (fileInput) {
+      fileInput.files = event.dataTransfer.files;
+      uploadFormRef.current.requestSubmit();
+    }
+  }, []);
+  useEffect(() => {
+    document.body.addEventListener("dragover", onDragOver);
+    document.body.addEventListener("dragleave", onDragLeave);
+    document.body.addEventListener("drop", onDrop);
+    return () => {
+      document.body.removeEventListener("dragover", onDragOver);
+      document.body.removeEventListener("dragleave", onDragLeave);
+      document.body.removeEventListener("drop", onDrop);
+    };
+  }, [onDragOver, onDragLeave, onDrop]);
+
   return (
     <div className="space-y-4">
-      <form
-        className="flex flex-col space-y-2 sm:flex-row sm:space-x-2 sm:space-y-0"
-        onSubmit={onSubmit}
-      >
-        <div className="relative w-full flex-1">
-          <input
-            type="text"
-            className={classNames(
-              "w-full px-5 py-3 text-lg rounded border-2 outline-none",
-              {
-                "text-indigo-800 bg-indigo-100 border-indigo-600 placeholder-indigo-200":
-                  !error,
-                "text-red-800 bg-red-100 border-red-700 focus:border-red-900":
-                  error,
-              }
-            )}
-            placeholder="https://scratch.mit.edu/projects/345789566/"
-            value={projectURL}
-            onChange={(e) => {
-              setProjectURL(e.target.value);
-              setError(null);
-            }}
-            onBlur={() => {
-              // Normalize url format
-              let id = getProjectId(projectURL);
-              if (id) {
-                setProjectURL(getProjectURL(id));
-              }
-            }}
-          />
-        </div>
+      <div className="space-y-2 relative">
+        {/* Project URL form */}
+        <form
+          className="flex flex-col space-y-2 sm:flex-row sm:space-x-2 sm:space-y-0"
+          onSubmit={onSubmit}
+        >
+          <div className="relative w-full flex-1">
+            <input
+              type="text"
+              className={classNames(
+                "w-full px-5 py-3 text-lg rounded border-2 outline-none",
+                {
+                  "text-indigo-800 bg-indigo-100 border-indigo-600 placeholder-indigo-200":
+                    !error,
+                  "text-red-800 bg-red-100 border-red-700 focus:border-red-900":
+                    error,
+                }
+              )}
+              placeholder="https://scratch.mit.edu/projects/345789566/"
+              value={projectURL}
+              onChange={(e) => {
+                setProjectURL(e.target.value);
+                setError(null);
+              }}
+              onBlur={() => {
+                // Normalize url format
+                let id = getProjectId(projectURL);
+                if (id) {
+                  setProjectURL(getProjectURL(id));
+                }
+              }}
+            />
+          </div>
 
-        <ProgressButton disabled={!projectId} loading={loading} error={!!error}>
-          Edit as JavaScript
-        </ProgressButton>
-      </form>
+          <ProgressButton
+            disabled={!projectId}
+            loading={loading}
+            error={!!error}
+          >
+            Edit as JavaScript
+          </ProgressButton>
+        </form>
+
+        {/* File upload form */}
+        <form
+          ref={uploadFormRef}
+          className="flex justify-center"
+          onSubmit={onSubmitUpload}
+        >
+          <label>
+            <span className="text-gray-700">
+              Or{" "}
+              <span className="text-indigo-800 font-medium underline decoration-dashed decoration-indigo-700/30 cursor-pointer">
+                upload
+              </span>{" "}
+              an .sb3 file
+            </span>
+            <input
+              type="file"
+              name="file"
+              className="hidden"
+              accept=".sb3"
+              onChange={(event) => {
+                uploadFormRef.current?.requestSubmit();
+              }}
+            />
+          </label>
+
+          <div
+            className={classNames("fixed inset-0 bg-indigo-400/20", {
+              hidden: !draggingFile,
+            })}
+          />
+          <div
+            className={classNames(
+              "absolute -inset-5 -top-14 bg-indigo-400 border-4 border-indigo-500 border-dashed rounded-xl shadow-2xl shadow-indigo-600/60 flex items-center justify-center",
+              { hidden: !draggingFile }
+            )}
+          >
+            <span className="text-xl font-semibold text-white drop-shadow-xl select-none">
+              Drop to upload sb3...
+            </span>
+          </div>
+        </form>
+      </div>
 
       {/* Error box */}
       {error && (
---
+++ pages/api/upload/codesandbox.ts
@@ -0,0 +1,134 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import fetch from "node-fetch";
+import { Project } from "sb-edit";
+import { getParameters } from "codesandbox/lib/api/define";
+import FormData from "form-data";
+import getRawBody from "raw-body";
+import JSZip from "jszip";
+import S3 from "aws-sdk/clients/s3";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const s3 = new S3({
+  endpoint: `https://${process.env.CLOUDFLARE_R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  accessKeyId: `${process.env.CLOUDFLARE_R2_KEY_ID}`,
+  secretAccessKey: `${process.env.CLOUDFLARE_R2_KEY_SECRET}`,
+  signatureVersion: "v4",
+});
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const body = await getRawBody(req);
+    const sb3File = body;
+
+    const zip = await JSZip.loadAsync(body);
+    const json = await zip.file("project.json").async("text");
+
+    // Upload assets to Cloudflare R2 for temporary storage
+    const getAsset = async ({
+      md5,
+      ext,
+    }): Promise<{ content: string; isBinary: boolean }> => {
+      if (ext === "svg") {
+        // No need to upload SVG files to R2. (And in fact, trying to do so
+        // is failing for me for some reason.) Instead, just pass the text content of the SVG file.
+        return {
+          content: await zip.file(`${md5}.${ext}`).async("text"),
+          isBinary: false,
+        };
+      }
+
+      // Get asset data from zip as a `Buffer`
+      const data = await zip.file(`${md5}.${ext}`).async("nodebuffer");
+
+      // Upload asset to Cloudflare R2
+      await s3
+        .putObject({
+          Bucket: "leopard-website",
+          Key: `uploaded-sb3-files/${md5}.${ext}`,
+          Body: data,
+          Expires: new Date(Date.now() + 5 * 60 * 1000), // Automatically deleted after 5 minutes
+        })
+        .promise();
+
+      // Return a signed URL that allows access to the file for the next 5 minutes
+      return {
+        content: await s3.getSignedUrlPromise("getObject", {
+          Bucket: "leopard-website",
+          Key: `uploaded-sb3-files/${md5}.${ext}`,
+          Expires: 5 * 60, // 5 minutes
+        }),
+        isBinary: true,
+      };
+    };
+
+    const project = await Project.fromSb3JSON(JSON.parse(json), { getAsset });
+
+    const converted = project.toLeopard({});
+
+    let files = {};
+
+    for (const fileName in converted) {
+      let content = converted[fileName];
+
+      // Prepend index.html with a comment
+      if (fileName === "index.html") {
+        content = `<!--
+  Congratulations! Your project has been converted to JavaScript!
+
+  You should see a working preview of your project on the right side of
+  the screen. If not, it means something went wrong.
+
+  Your project's code is all stored in the files on the left side of the
+  screen. You can edit the JavaScript code and your project will change.
+  But you have to save your changes using File > Save or Ctrl+S on your
+  keyboard before the project preview will update.
+-->
+
+${content}`;
+      }
+
+      // Prepend sprite JS files with eslint-disable
+      if (/.+\/.+\.js/g.test(fileName)) {
+        content = "/* eslint-disable require-yield, eqeqeq */\n\n" + content;
+      }
+
+      files[fileName] = { content, isBinary: false };
+    }
+
+    for (const target of [project.stage, ...project.sprites]) {
+      for (const costume of target.costumes) {
+        files[`${target.name}/costumes/${costume.name}.${costume.ext}`] =
+          costume.asset;
+      }
+      for (const sound of target.sounds) {
+        files[`${target.name}/sounds/${sound.name}.${sound.ext}`] = sound.asset;
+      }
+    }
+
+    const formData = new FormData();
+    formData.append("parameters", getParameters({ files }));
+    formData.append("json", 1);
+
+    const result: any = await fetch(
+      "https://codesandbox.io/api/v1/sandboxes/define",
+      {
+        method: "POST",
+        body: formData,
+      }
+    ).then((res) => res.json());
+
+    const sandboxId = result.sandbox_id;
+
+    res
+      .status(200)
+      .json({ url: `https://codesandbox.io/s/${sandboxId}?file=/index.js` });
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json({ error: err.message });
+  }
+};
