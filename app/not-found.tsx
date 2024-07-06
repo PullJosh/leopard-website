77c36d85e0363350c82e2d011f7f9506db86e0e9
Josh Pullen <JoshPullen27@gmail.com>
2024-07-05 20:04:30 -0400

    Fix design of 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,21 +1,24 @@
+import Center from "../components/Center";
 import { Footer } from "../components/Footer";
 import Nav from "../components/Nav";
+import TopBorder from "../components/TopBorder";
 
 export default function NotFound() {
   return (
-    <>
-      <div className="border-b border-gray-300">
-        <Nav title="Leopard" />
-      </div>
-      <div className="mt-8">
-        <div className="mx-auto max-w-4xl px-8">
-          <h1 className="text-4xl font-bold">404 - Not Found</h1>
-          <p className="mt-4">
-            The page you were looking for could not be found.
-          </p>
+    <div className="flex min-h-screen flex-col">
+      <TopBorder />
+      <Nav title="Leopard" />
+      <div className="flex-grow">
+        <div className="my-8">
+          <Center>
+            <div className="prose">
+              <h1>404 - Not Found</h1>
+              <p>The page you were looking for could not be found.</p>
+            </div>
+          </Center>
         </div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
